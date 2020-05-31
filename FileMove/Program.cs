ff090ae7d5aec5b13a71b620f540a8a0ab261205
HY Z <hoyu_z@163.com>
2020-05-31 23:54:22 +0800

    添加文件操作小工具

---
+++ FileMove/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileMove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                    return;
+
+                var fileNames = new DirectoryInfo(args[0]).GetFiles();
+                fileNames.ToList().ForEach(d =>
+                {
+
+                    string name = d.Name;
+                    name = name.Replace('(', '_').Replace(')', '_').Replace(' ', '_').Substring(0, name.LastIndexOf('.'));
+                    string dir = d.DirectoryName + "/" + name;
+                    if (!Directory.Exists(dir))
+                        Directory.CreateDirectory(dir);
+                    d.MoveTo(d.Directory + "/" + name + "/" + name + d.Extension);
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
